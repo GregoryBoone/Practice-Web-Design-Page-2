f6b1559de6811d0a929ab165dc60a5cd7affc9c6
Micah Boone <mboone137@gmail.com>
2022-10-25 19:55:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const colorToggle = document.getElementById('link');
+
+colorToggle.addEventListener('mouseover', function handleMouseOver(){colorToggle.style.color = '#fff';});
+
+colorToggle.addEventListener('mouseout', function handleMouseOver(){colorToggle.style.color = '#9198e5';});
